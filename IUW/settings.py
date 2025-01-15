a063abeb73e957161980dba03ce993e7fb28c4a5
Thanh Lee <thanh@cloudbinfies.com>
2025-01-15 15:19:57 +0000

    update docker and env

--- IUW/settings.py
+++ IUW/settings.py
@@ -94,9 +94,24 @@ WSGI_APPLICATION = 'IUW.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': env.db(default='sqlite:////' + str(BASE_DIR / 'db.sqlite3')),
-}
+if env('DJANGO_ENV', default='development') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('DB_NAME'),
+            'USER': env('DB_USER'),
+            'PASSWORD': env('DB_PASSWORD'),
+            'HOST': env('DB_HOST', default='localhost'),
+            'PORT': env('DB_PORT', default='5432'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
